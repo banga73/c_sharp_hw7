68ce954a3cd835a9ebc62bac2c07b9c96a9c6ffb
banga73 <banga73@gmail.com>
2023-07-01 23:38:59 +0300

    50

--- task50.cs
+++ task50.cs
@@ -11,18 +11,15 @@ double[,] RandomTwoArray(int UserRows, int UserColomns, int Min, int Max)
 	}
 	return IntArray;
 }
-void PrintTwoArray(double[,] TwoArray)
-{
-	for (int i = 0; i < TwoArray.GetLength(0); i++)
-	{
-		for (int j = 0; j < TwoArray.GetLength(1); j++)
-			Console.Write($"{TwoArray[i,j]} ");
-		Console.WriteLine();
-	}
-}
+Console.Write("Input X and Y of element with a space between ");
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);		
+int UserX = int.Parse(st[0]);
+int UserY = int.Parse(st[1]);
 Console.Write("Input quantity of rows ");
 int UserRows = int.Parse(Console.ReadLine());
 Console.Write("Input quantity of colomns ");
 int UserColomns = int.Parse(Console.ReadLine());
 double[,] OurArray = RandomTwoArray(UserRows, UserColomns, -1000, 1000);
-PrintTwoArray(OurArray);
+if (UserX <= OurArray.GetLength(0) && UserY <= OurArray.GetLength(1))
+	Console.Write($"Element is {OurArray[UserX, UserY]}");
+else Console.Write("There is no such element");	
